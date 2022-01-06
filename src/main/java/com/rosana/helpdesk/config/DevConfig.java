4200c4cd46d3ddc3c199d97669ac1538d1a7f718
rosana <rosanariblim@gmail.com>
2022-01-06 12:22:09 -0300

    criando perfil de desenvolvimento

--- src/main/java/com/rosana/helpdesk/config/DevConfig.java
+++ src/main/java/com/rosana/helpdesk/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.rosana.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,15 +11,22 @@ import com.rosana.helpdesk.services.DBService;
 //aqui eu quero que, toda vez que o perfil de teste estiver ativo, ele suba os dados de teste no BD
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
 	@Bean
-	public void instanciaDB() {
-		this.dbService.instanciaDB();
+	public boolean instanciaDB() {
+		if (value.equals("create")) {
+			this.dbService.instanciaDB();
+			return true;
+		}
+		return false;
 	}
 
 }
--- src/main/java/com/rosana/helpdesk/services/DBService.java
+++ src/main/java/com/rosana/helpdesk/services/DBService.java
@@ -29,18 +29,29 @@ public class DBService {
 		
 			//----AQUI ESTAMOS CRIANDO, MAS AINDA NÃO É SALVO NO BD.
 			Tecnico tec1 = new Tecnico(null, "Valdir cezar", "63653230268", "valdir@mail.com", "123");
+			Tecnico tec2 = new Tecnico(null, "Rosana", "12345678901", "rosana@mail.com", "456");
+			
 			tec1.addPerfil(Perfil.ADMIN);
+			tec2.addPerfil(Perfil.ADMIN);
+			
 			Cliente cli1 = new Cliente(null, "Linus Torvalds", "80527954780", "torvalds@mail.com","123");
+			Cliente cli2 = new Cliente(null, "Xuxa", "00000012312", "xuxa@mail.com","456");
+			Cliente cli3 = new Cliente(null, "Peter pan", "90958239223", "peter@mail.com", "789");
+			
 			Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado",tec1,cli1);
+			Chamado c2 = new Chamado(null, Prioridade.BAIXA, Status.ABERTO, "Chamado 02", "Segundo chamado",tec1,cli2);
+			Chamado c3 = new Chamado(null, Prioridade.ALTA, Status.ANDAMENTO, "Chamado 03", "Terceiro chamado",tec1,cli3);
+			Chamado c4 = new Chamado(null, Prioridade.ALTA, Status.ENCERRADO, "Chamado 04", "Quarto chamado",tec2,cli2);
+			Chamado c5 = new Chamado(null, Prioridade.MEDIA, Status.ABERTO, "Chamado 05", "Quinto chamado",tec2,cli1);
 								//o perfil de cliente já é gerado automaticamente em qualquer instância de cliente (ou técnico) pelo próprio construtor, 
 								//então não precisamos adicionar aqui.
 			
 			//----AGORA SIM VAMOS SALVAR NO BD
 			//no código abaixo, se tivesse mais de um técnico, poderia fazer
 			//tecnicoRepository.saveAll(Arrays.asList(tec1,tec2,tec3,...));
-			tecnicoRepository.saveAll(Arrays.asList(tec1));
-			clienteRepository.saveAll(Arrays.asList(cli1));
-			chamadoRepository.saveAll(Arrays.asList(c1));
+			tecnicoRepository.saveAll(Arrays.asList(tec1, tec2));
+			clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3));
+			chamadoRepository.saveAll(Arrays.asList(c1,c2,c3,c4,c5));
 		}
 
 }
