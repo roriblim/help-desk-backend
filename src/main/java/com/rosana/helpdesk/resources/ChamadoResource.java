541b72a18dc6869bb1725b7e3ceadcc2087a0935
rosana <rosanariblim@gmail.com>
2022-01-07 16:49:56 -0300

    findAll para chamados

--- src/main/java/com/rosana/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/rosana/helpdesk/resources/ChamadoResource.java
@@ -1,5 +1,8 @@
 package com.rosana.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,4 +26,12 @@ public class ChamadoResource {
 		Chamado obj = service.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll(){
+		List<Chamado> list = service.findAll();
+		List<ChamadoDTO> listDTO = list.stream().map(x-> new ChamadoDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
 }
--- src/main/java/com/rosana/helpdesk/services/ChamadoService.java
+++ src/main/java/com/rosana/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.rosana.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,11 @@ public class ChamadoService {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
 		
+	}
+
+	public List<Chamado> findAll() {
+		// TODO Auto-generated method stub
+		return repository.findAll();
 	}
 	
 }
