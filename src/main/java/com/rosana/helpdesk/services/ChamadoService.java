e15c913d97923cd9859c10d5b2af42bd649401fa
rosana <rosanariblim@gmail.com>
2022-01-09 10:37:58 -0300

    endpoint update para Chamado

--- src/main/java/com/rosana/helpdesk/services/ChamadoService.java
+++ src/main/java/com/rosana/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.rosana.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,22 +44,38 @@ public class ChamadoService {
 		return repository.save(newChamado(objDTO));
 	}
 	
-	private Chamado newChamado(ChamadoDTO obj) {
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		// TODO Auto-generated method stub
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
+	}
+	
+	private Chamado newChamado(ChamadoDTO objDTO) {
+		//este método vai validar alguns dados no chamado a ser criado, bem como criar um Chamado a partir
+		//de um ChamadoDTO
+		
 		//quando eu puxo o findById desses services, se não existir, uma exceção vai ser lançada lá dentro do findById
-		Tecnico tecnico = tecnicoService.findById(obj.getTecnico()); //o campo tecnico em ChamadoDTO é na verdade o id do técnico
-		Cliente cliente = clienteService.findById(obj.getCliente());
+		Tecnico tecnico = tecnicoService.findById(objDTO.getTecnico()); //o campo tecnico em ChamadoDTO é na verdade o id do técnico
+		Cliente cliente = clienteService.findById(objDTO.getCliente());
 		Chamado chamado = new Chamado();
-		if (obj.getId() !=null) {
-			chamado.setId(obj.getId());
+		if (objDTO.getId() !=null) {
+			chamado.setId(objDTO.getId());
+		}
+		if((objDTO.getStatus().equals(2)) && (objDTO.getDataFechamento()==null)) {
+			chamado.setDataFechamento(LocalDate.now());
 		}
 		chamado.setTecnico(tecnico);
 		chamado.setCliente(cliente);
-		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
-		chamado.setStatus(Status.toEnum(obj.getStatus()));
-		chamado.setTitulo(obj.getTitulo());
-		chamado.setObservacoes(obj.getObservacoes());
+		chamado.setPrioridade(Prioridade.toEnum(objDTO.getPrioridade()));
+		chamado.setStatus(Status.toEnum(objDTO.getStatus()));
+		chamado.setTitulo(objDTO.getTitulo());
+		chamado.setObservacoes(objDTO.getObservacoes());
 		
 		return chamado;
 	}
+
+
 	
 }
